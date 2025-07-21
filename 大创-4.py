cf8badfcb3f1bb2a1a0c2bd26fe107f90fe157c2
cceennjjiinn <1718126631@qq.com>
2025-07-21 15:58:42 +0800

    Update 大创-4.py

--- 大创-4.py
+++ 大创-4.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter
-import sympy as sp
+from sympy import symbols, Eq, solve
 from io import BytesIO
 from PIL import Image
 import itertools
